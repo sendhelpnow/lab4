d9a096d2a22da48d6f31ca1b85a5008de11f8de8
clai <clai-@hotmail.com>
2018-02-01 16:20:32 -0800

    rerouted hotel to go to golf's controller, closes issue #8

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,3 +53,6 @@ $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['foxtrot'] = 'tango';
+$route['bananas/rule'] = function() {
+    return 'golf';
+};
